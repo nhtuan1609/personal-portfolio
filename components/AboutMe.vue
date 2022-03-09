73798227632b1865360bf72cca01abc987fe7321
Nguyen Hoang Tuan <nhtuan1609@gmail.com>
2022-03-10 05:24:30 +0700

    create AboutMe.vue component

---
+++ components/AboutMe.vue
@@ -0,0 +1,49 @@
+<template>
+  <session-content>
+    <template #title>ABOUT ME</template>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="6" class="d-flex justify-center">
+          <v-img
+            class="about__image"
+            max-width="80%"
+            contain
+            :src="require('~/static/profile/avatar.jpg')"
+            alt="avatar-image"
+          />
+        </v-col>
+        <v-col cols="12" md="6" class="d-flex align-center">
+          <div class="about__information">
+            Hi there! <strong>Nguyen Hoang Tuan</strong> is my full name. I was born and brought up in Can Tho city. And
+            now I am working as an Web Developer.
+            <br />
+            <br />I love learning about new technologies, facing problems and solving them, and how I can use them to
+            create better and scalable products. I am looking for an environment where I can show all my technical
+            skills.
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </session-content>
+</template>
+
+<script>
+import SessionContent from '~/components/commons/SessionContent.vue'
+
+export default {
+  name: 'AboutMe',
+  components: { SessionContent }
+}
+</script>
+
+<style lang="scss" scoped>
+.about {
+  &__image {
+    border-radius: 4px;
+  }
+  &__information {
+    text-align: justify;
+    font-size: 1.5rem;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,17 @@
   <v-layout fill-height>
     <v-container style="margin-top: -80px">
       <home-message id="home" />
+      <about-me id="about" />
     </v-container>
   </v-layout>
 </template>
 
 <script>
 import HomeMessage from '~/components/HomeMessage.vue'
+import AboutMe from '~/components/AboutMe.vue'
 
 export default {
   name: 'HomePage',
-  components: { HomeMessage }
+  components: { HomeMessage, AboutMe }
 }
 </script>
